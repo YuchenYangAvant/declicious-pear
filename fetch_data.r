1e163826744f1619fbb410045e7d99013b060cdb
Yuchen Yang <yuchenyang@Avants-MacBook-Pro-5.local>
2015-04-06 09:55:05 -0500

    added cool stuff

---
+++ fetch_data.r
@@ -0,0 +1,23 @@
+
+
+
+fetch_historical_data <- function(peroid=60, days, ticker, type=c('d,c')) 
+  
+  {
+  url <- paste0('http://www.google.com/finance/getprices?i=',peroid,'&p=',days,'d&f=',type,'&df=cpct&q=',ticker)
+  data <-read.table(url,skip=7,sep=",",col.names=strsplit(type,split=",")[[1]])
+  if(grep('d',names(data))>0)
+  {
+  data$d<-as.character(data$d)
+  for (i in 1:dim(data)[1]){
+    if(grepl('[a-z]',data$d[i])){
+      date <- as.numeric(substr(as.character(data$d[i]),2,8))*1000
+      data$d[i] <- date
+    }else{ date+ as.numeric(data$d[i])->data$d[i]}
+  }
+  data$d<-as.numeric(data$d)  
+  }
+  data
+}
+
+
---
+++ plot_with_boundary.r
@@ -0,0 +1,28 @@
+plot_with_boundary <- function(data,target_name=c("ratio"),date_name='d',ma_period=100, n_sd=1){
+
+  if (!is.data.frame(data)) {
+    stop("data must be a data frame")
+  }
+  
+  data$std<-0
+  data$mean<-0
+  for(i in ma_period:(dim(data)[1]-1))
+  {
+    data$std[i]<-sd(data[[target_name]][(i-ma_period+1):(i-1)])
+    data$mean[i]<-mean(data[[target_name]][(i-ma_period+1):(i-1)]) 
+  }
+
+  data$plus_std <- data$mean+data$std
+  data$minus_std <- data$mean-data$std
+  
+  data$plus_2std <- data$mean+data$std*2
+  data$minus_2std <- data$mean-data$std*2
+  
+  plot (data[[target_name]], type='l')
+  lines(data$plus_std, type='l', col='red')
+  lines(data$plus_2std, type='l', col='red')
+  lines(data$minus_std, type='l', col='green')
+  lines(data$minus_2std, type='l', col='green')
+  
+}
+
