1649384ababbe403bcf094f9db1bd1baaaa964ce
Yuchen Yang <yuchenyang@Avants-MacBook-Pro-5.local>
2015-04-12 16:40:41 -0500

    added plot function

--- plot_with_boundary.r
+++ plot_with_boundary.r
@@ -6,7 +6,7 @@ plot_with_boundary <- function(data,target_name=c("ratio"),date_name='d',ma_peri
   
   data$std<-0
   data$mean<-0
-  for(i in ma_period:(dim(data)[1]-1))
+  for(i in ma_period:(dim(data)[1]))
   {
     data$std[i]<-sd(data[[target_name]][(i-ma_period+1):(i-1)])
     data$mean[i]<-mean(data[[target_name]][(i-ma_period+1):(i-1)]) 
@@ -15,8 +15,8 @@ plot_with_boundary <- function(data,target_name=c("ratio"),date_name='d',ma_peri
   data$plus_std <- data$mean+data$std
   data$minus_std <- data$mean-data$std
   
-  data$plus_2std <- data$mean+data$std*2
-  data$minus_2std <- data$mean-data$std*2
+  data$plus_2std <- data$mean+data$std*1.65
+  data$minus_2std <- data$mean-data$std*1.65
   
   plot (data[[target_name]], type='l')
   lines(data$plus_std, type='l', col='red')
